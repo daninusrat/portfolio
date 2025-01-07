614c509235d91eee4bdce892028252a70053373a
Daniyal <danu0001@stud.kea.dk>
2025-01-07 03:00:43 +0100

    gitignore

--- js/burger.js
+++ js/burger.js
@@ -1,14 +1,9 @@
+// Vælg burger-ikon og menu
 const burger = document.querySelector(".burger");
-const nav = document.querySelector("nav");
 const menu = document.querySelector(".menu");
 
-burger.addEventListener("click", burgerClick);
-function burgerClick() {
+// Når burger-menuen klikkes
+burger.addEventListener("click", () => {
   burger.classList.toggle("active");
-  nav.classList.toggle("active");
-}
-menu.addEventListener("click", menuClick);
-function menuClick() {
-  burger.classList.remove("active");
-  nav.classList.remove("active");
-}
+  menu.classList.toggle("active");
+});
